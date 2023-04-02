6ac641fb28ccc9773425703ca5e3dc477fc04600
Sharoze Archer <sharoze.archer@gmail.com>
2023-04-01 23:59:20 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+function findMatching(drivers, string){
+    const result = drivers.filter(name => name.toLowerCase() === string.toLowerCase());
+    return result;
+}
+
+
+function fuzzyMatch(drivers, letter){
+    const result = drivers.filter(name => name.indexOf(letter) === 0);
+    return result;
+}
+
+
+function matchName(drivers, string){
+    const result = drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+    return result;
+}
